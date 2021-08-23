2195ec261bec16fa30aaa3e392de6c3db1114dd3
MartisiusL <31843212+MartisiusL@users.noreply.github.com>
2021-08-23 13:03:51 +0300

    Leave only one active solution

--- PathFinder/Program.cs
+++ PathFinder/Program.cs
@@ -4,7 +4,7 @@
         {
         static void Main (string[] args)
             {
-            SolutionWithJaggedArrays.SolutionWithJaggedArrays.Run ();
+            // SolutionWithJaggedArrays.SolutionWithJaggedArrays.Run ();
             SolutionWithBinaryTree.SolutionWithBinaryTree.Run ();
             }
         }
